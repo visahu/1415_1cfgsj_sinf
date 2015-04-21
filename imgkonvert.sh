2729dc2b87bd363084a36189c296e9da693f190d
laguna9loire <laguna9loire@users.noreply.github.com>
2015-04-21 13:47:05 +0200

    Update imgkonvert.sh
    original

--- imgkonvert.sh
+++ imgkonvert.sh
@@ -9,16 +9,19 @@
 # Main code
 
 ls -1 $1*.jpg > imagefiles
-
+cat imagefiles
 while read imgfile 
 do
+	echo "IMG File: $imgfile"
 	lengthname=${#imgfile}
+	echo "lenght: $lenghtname"
 	namefile=${imgfile;0;$lengthname-4}
+	echo "namefile: $namefile"
 	convert $imgfile -resize 200x200 $namefile.png	
-	convert $imgfile -rotate 90 $namefile.jpg
+	convert $imgfile -rotate 90 $namefile.png
 	convert $imgfile -font helvetica -fill white -pointsize 36 -draw ‘text 10,50 “pretty kitties…”$namefile.gif
 
-done < imagefile
+done < imagefiles
 rm imagefiles
 exit 0
  
