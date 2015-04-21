04a4b4254623b556cd7b0c5d92e3f37ea76dcfbb
visahu <visahu@gmail.com>
2015-04-21 13:51:14 +0200

    Update imgkonvert.sh

--- imgkonvert.sh
+++ imgkonvert.sh
@@ -17,7 +17,7 @@ do
 	lengthname=${#imgfile}
 	namefile=${imgfile:0:$lengthname-4}
 	echo $namefile
-	#convert $imgfile -resize 200x200 "$namefile".png	
+	convert $imgfile -resize 200x200 "$namefile".png	
 	convert $imgfile -rotate 90 "$namefile".jpg
 	convert $imgfile -font helvetica -fill white -pointsize 36 -draw ‘text 10,50 “pretty babies…”"$namefile".gif
 	convert $imgfile.png -colorspace Gray "$namefile".png
